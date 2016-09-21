76ee3c057df614e9a7bfcded3df618611703667d
zaturnvalley <zacharycmason@gmail.com>
2016-09-21 11:43:46 -0700

    built AirplaneShowCtrl and templateUrl for airplanes show state

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -2,6 +2,23 @@ angular.module('AirplanesCtrls', ['AirplanesFactories'])
 .controller('AirplanesCtrl', ['$scope', 'AirplanesAPI', function($scope, AirplanesAPI) {
   $scope.airplanes = [];
 
+  AirplanesAPI.query(function success(res) {
+    $scope.airplanes = res;
+  }, function error(res) {
+    console.log(res);
+  });
+}])
+.controller('AirplaneShowCtrl', ['$scope', 'AirplanesAPI', '$stateParams', function($scope, AirplanesAPI, $stateParams) {
+  $scope.airplane = {};
+
+  AirplanesAPI.get({id: $stateParams.id}, function success(res) {
+    $scope.airplane = res;
+  }, function error(res) {
+    console.log(res);
+  });
+}]);
+
+//If wanted, here is how the $http req would look:
   // $http({
   //   url: '/api/airplanes',
   //   method: 'GET'
@@ -10,10 +27,3 @@ angular.module('AirplanesCtrls', ['AirplanesFactories'])
   // }, function error(res) {
   //   console.log(res);
   // });
-
-  AirplanesAPI.query(function success(res) {
-    $scope.airplanes = res;
-  }, function error(res) {
-    console.log(res);
-  });
-}]);
